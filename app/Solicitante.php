37eff10ed0d065f2b16474ea76c4adb7c1b553a9
MartínTrujillo Bustamante <martin.trujillo1105@gmail.com>
2020-08-15 20:13:08 -0500

    change in BD

--- app/Solicitante.php
+++ app/Solicitante.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Solicitante extends Model
 {
+    protected $primaryKey = 'id';
+    
     protected $table = 'solicitante';
 
-    protected $fillable = ['nombre','ruc','direccion', 'telefono', 'email'];
+    protected $fillable = ['SOLIC_Nombre', 'SOLIC_Ruc', 'SOLIC_Direccion', 'SOLIC_Telefono', 'SOLIC_Email','SOLIC_FlagEstado','TIPSOLIP_Codigo','UBIGP_Codigo'];
 
-    //public $timestamps = false;
+    public $timestamps = false;
 
 }
