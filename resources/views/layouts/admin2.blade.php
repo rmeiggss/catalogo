e7a4e859608c1b8e875b6f86a20860d2cd32be02
edwardfigueroa <edward.figueroa.ing@gmail.com>
2020-08-18 22:56:27 -0500

    Colores UNI INTRANET NUEVO
    COLORES UNI INTRANET NUEVO

--- resources/views/layouts/admin2.blade.php
+++ resources/views/layouts/admin2.blade.php
@@ -149,14 +149,14 @@
    <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ asset('home') }}" class="brand-link">
+    <a href="{{ asset('home') }}" class="brand-link" style="background-color: rgba(128,4,4,0.7)">
       <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">ELECTRILAB</span>
     </a>
 
     <!-- Sidebar -->
-    <div class="sidebar">
+    <div class="sidebar" style="background-color: rgba(128,4,4,0.7)">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
