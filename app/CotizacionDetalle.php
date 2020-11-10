0fa7279d48fc9befa98c35a0bad2a9a594293106
MartínTrujillo Bustamante <martin.trujillo1105@gmail.com>
2020-11-10 07:59:56 -0500

    a

--- app/CotizacionDetalle.php
+++ app/CotizacionDetalle.php
@@ -10,7 +10,18 @@ class CotizacionDetalle extends Model
     
     protected $table = 'cotizacion_detalle';
     
-    protected $fillable = ['CODEC_NombreEquipo', 'CODEC_Descripcion', 'CODEC_Fabricante', 'CODEC_Url', 'CODEC_Cantidad','CODEC_SubTotal','CODEC_PrecioUnitario','COTIP_Codigo'];
+    protected $fillable = [
+        'CODEC_NombreEquipo', 
+        'CODEC_Descripcion', 
+        'CODEC_Fabricante', 
+        'CODEC_Url', 
+        'CODEC_Cantidad',
+        'CODEC_SubTotal',
+        'CODEC_PrecioUnitario',
+        'COTIP_Codigo',
+        'CODEC_Descripcion_Ficha_Tecnica_Equipo',
+        'CODEC_Archivo_Descripcion_Equipo'
+    ];
 
     public $timestamps = false;
 
